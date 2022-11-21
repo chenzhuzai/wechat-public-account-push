5bc20b314e407fa702bd008e24ec17edd1d39e73
chenzhuzai <79857844+chenzhuzai@users.noreply.github.com>
2022-11-21 13:34:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小陈',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTju86UtuJMhPO2r53k3TDtYZgRM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'e99kYgC0ZDcyCUVhCpX3UzAJ_In1h_dscHCukJb9Jsk',
+      useTemplateId: '	e99kYgC0ZDcyCUVhCpX3UzAJ_In1h_dscHCukJb9Jsk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -64,3 +64,4 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
+
